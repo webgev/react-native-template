92d5cfdcf01e6fae69c7be8fed665bf60ec2a553
webgev <webgev@yandex.ru>
2021-06-10 13:47:15 +0300

    Create template

---
+++ template/babel.config.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const alias = { '~': path.resolve(__dirname, 'src') }
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [['module-resolver', { alias }]],
+}
